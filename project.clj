f4f2e0e7221a40c31075e818e7929e6d977c9fa5
Ben Englert <ben.englert@gmail.com>
2011-03-14 23:29:51 -0400

    Put my clojars name into project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beanstalk "1.0.1"
+(defproject org.clojars.bengl3rt/beanstalk "1.0.1"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
