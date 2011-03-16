1d831cbd8c4d576367c04ec0bc92e08459532225
Ben Englert <ben.englert@gmail.com>
2011-03-16 02:00:05 -0400

    Adding yaml library dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.bengl3rt/beanstalk "1.0.1"
-  :description "FIXME: write"
+(defproject org.clojars.bengl3rt/beanstalk "1.0.3"
+  :description "bengl3rt's fork of the clojure beanstalk library"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-yaml "0.3.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   ; :main [namespace.with.main]
   ; :run-aliases {:one another.namespace :two yet.another.namespace}
