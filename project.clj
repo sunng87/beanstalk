9f5d16839bdd7ea7b597045ccb209973870783ef
Sun Ning <classicning@gmail.com>
2011-08-27 20:51:12 +0800

    remove snapshots dependencoes

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject org.clojars.sunng/beanstalk "1.0.4"
   :description "sunng's fork of the clojure beanstalk library"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-yaml "0.3.0-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]]
   )
