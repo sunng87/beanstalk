939234a37e1b4b0035e621f554dddb44d3653dcb
Sun Ning <classicning@gmail.com>
2011-09-25 18:10:59 +0800

    upgrade clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.clojars.sunng/beanstalk "1.0.5"
+(defproject org.clojars.sunng/beanstalk "1.0.6"
   :description "sunng's fork of the clojure beanstalk library"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [gfrlog/clj-yaml "0.3.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]]
   )
